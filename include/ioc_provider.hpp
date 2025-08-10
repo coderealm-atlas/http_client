710a1a5053282a2c218ef4cf7eb5ddfa6d2f9243
jianglibo <jianglibo@hotmail.com>
2025-08-10 05:57:53 +0000

    minor change.

--- include/ioc_provider.hpp
+++ include/ioc_provider.hpp
@@ -6,6 +6,15 @@ namespace ioc {
 class IIocProvider {
  public:
   virtual ~IIocProvider() = default;
-  virtual boost::asio::io_context& get(const char* name) = 0;
+  virtual boost::asio::io_context& get() = 0;
 };
+
+class DummyIocProvider : public IIocProvider {
+ public:
+  boost::asio::io_context& get() override {
+    static boost::asio::io_context dummy_ioc;
+    return dummy_ioc;
+  }
+};
+
 }  // namespace ioc
